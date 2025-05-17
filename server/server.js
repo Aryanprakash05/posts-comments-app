4bd7707276358da6f211da330cb66ed94d081422
Aryanprakash05 <145462392+Aryanprakash05@users.noreply.github.com>
2025-05-17 13:30:00 +0530

    Ready for deploy to Render

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: './server/.env' });
 
 const app = express();
 app.use(cors());
